2c3b30733ea5c39cba0a890f1966d978bc82eac4
Maximo Fernandez Riera <mfernandez@cifpfbmoll.eu>
2021-11-07 20:37:14 +0100

    One pass and one fails.

--- test/index_test.js
+++ test/index_test.js
@@ -4,8 +4,34 @@ var Calculate =  require('../index.js')
 describe('Calculate', () => {
   describe('.factorial', () => {
     it('returns the correct value of 5!', () => {
-        assert.equal(Calculate.factorial(5), 120);
+      // Setup
+
+      const inputNumber = 5;
+      const expectedResult = 120;
+      
+      // Test logic
+
+      const result = Calculate.factorial(inputNumber);
+
+      // Verify
+      
+      assert.equal(result, expectedResult);
       });
+
+      it('returns the correct value of 3!', () => {
+        // Setup
+  
+        const inputNumber = 3;
+        const expectedResult = 6;
+        
+        // Test logic
+  
+        const result = Calculate.factorial(inputNumber);
+  
+        // Verify
+        
+        assert.equal(result, expectedResult);
+        });
 
   });
 });
