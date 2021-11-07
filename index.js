7110108821b1d4b555327402eb00952afaa21277
Maximo Fernandez Riera <mfernandez@cifpfbmoll.eu>
2021-11-07 20:44:09 +0100

    Refactoring the multiplication.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Calculate = {
     factorial(inputNumber){
         for (let i = inputNumber -1 ; i>=1; i--){
-            inputNumber = inputNumber *i
+            inputNumber *= i;
         }
         return inputNumber;
     }
